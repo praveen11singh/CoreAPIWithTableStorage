a96396fe410395743aa21e6345da546537bb29a5
praveen11singh <praveen11_singh@yahoo.co.in>
2021-12-22 09:16:20 +0000

    First commit

---
+++ CoreAPIWithTableStorage/Model/GroceryItemEntity.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+
+namespace CoreAPIWithTableStorage.Model
+{
+    public class GroceryItemEntity : TableEntity
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ CoreAPIWithTableStorage/Services/GroceryStoreAPI.cs
@@ -0,0 +1,14 @@
+﻿using CoreAPIWithTableStorage.Model;
+using System.Threading.Tasks;
+
+namespace CoreAPIWithTableStorage.Services
+{
+    
+        public interface ITableStorageService
+        {
+            Task<GroceryItemEntity> RetrieveAsync(string category, string id);
+            Task<GroceryItemEntity> InsertOrMergeAsync(GroceryItemEntity entity);
+            Task<GroceryItemEntity> DeleteAsync(GroceryItemEntity entity);
+        }
+  
+}
